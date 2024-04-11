5c9d1f8ee85105ede0d0940d798aef55d6b530dd
Alexandre Costa <alex@alexcosta.dev>
2024-04-11 07:12:13 +0100

    feat(lockfile): add basic lockfile check logic

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,8 @@
-import getPRTitle from './index';
+import { PackageManager, checkLockfileUpdate, getPRTitle } from './index';
 
 declare const global: any;
 
-describe('getPRTitle()', () => {
+describe('index', () => {
   beforeEach(() => {
     global.warn = jest.fn();
     global.message = jest.fn();
@@ -17,13 +17,32 @@ describe('getPRTitle()', () => {
     global.markdown = undefined;
   });
 
-  it('Checks for a that message has been called', () => {
-    global.danger = {
-      github: { pr: { title: 'My Test Title' } }
-    };
+  describe('getPRTitle()', () => {
+    it('Checks for a that message has been called', () => {
+      global.danger = {
+        github: { pr: { title: 'My Test Title' } }
+      };
 
-    getPRTitle();
+      getPRTitle();
 
-    expect(global.message).toHaveBeenCalledWith('PR Title: My Test Title');
+      expect(global.message).toHaveBeenCalledWith('PR Title: My Test Title');
+    });
+  });
+
+  describe('checkLockfileUpdate()', () => {
+    it('Sends a warning when NPM lockfile is not updated', () => {
+      global.danger = {
+        git: {
+          fileMatch: jest
+            .fn()
+            .mockReturnValueOnce({ modified: true })
+            .mockReturnValueOnce({ modified: false })
+        }
+      };
+
+      checkLockfileUpdate(PackageManager.NPM);
+
+      expect(global.warn).toHaveBeenCalledWith('You might have forgotten to run `npm install`');
+    });
   });
 });
